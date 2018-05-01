87978e15dd07f23a9cafea439996d873a04d36d8
BAN Jun <banjun@gmail.com>
2018-05-01 16:54:13 +0900

    refine optimized apply for UITableView by just reloading sections

--- BigDiffer/Classes/Differ/UITableView.swift
+++ BigDiffer/Classes/Differ/UITableView.swift
@@ -1,33 +1,73 @@
 import UIKit
 import Differ
 
+public enum Threshold {
+    public static let maxRowsToCalculateDiffs = 3000 // Differ cost: O((N+M)*D)
+    public static let maxDeletionsPreservingAnimations = 300 // UITableView.endUpdates cost: dominated by deletions
+}
+
 extension UITableView {
     public func applyWithOptimizations(
         _ diff: ExtendedDiff,
-        numberOfRows: Int,
         deletionAnimation: UITableViewRowAnimation = .automatic,
         insertionAnimation: UITableViewRowAnimation = .automatic,
-        indexPathTransform: (IndexPath) -> IndexPath = { $0 }
+        indexPathTransform: (IndexPath) -> IndexPath = { $0 },
+        maxDeletionsPreservingAnimations: Int = Threshold.maxDeletionsPreservingAnimations
         ) {
         let update = BatchUpdate(diff: diff, indexPathTransform: indexPathTransform)
 
-        beginUpdates()
-        defer {endUpdates()}
-
         // number of deletions affect tableview performance exponentially including non-visible deletions
-        if update.deletions.count < 300 {
+        let fallback = update.deletions.count > maxDeletionsPreservingAnimations
+
+        if fallback {
+            // animations no longer meaningful
+            UIView.performWithoutAnimation {
+                beginUpdates()
+                defer {endUpdates()}
+
+                reloadSections([0], with: .none)
+            }
+        } else {
+            beginUpdates()
+            defer {endUpdates()}
+
             deleteRows(at: update.deletions, with: deletionAnimation)
             insertRows(at: update.insertions, with: insertionAnimation)
             update.moves.forEach { moveRow(at: $0.from, to: $0.to) }
-        } else {
-            let recoveredInsertions: [IndexPath] = (0..<numberOfRows)
-                .map {IndexPath(row: $0, section: 0)}
-                .filter {!update.deletions.contains($0)}
-
-            deleteSections([0], with: .none)
-            insertSections([0], with: .none)
-            insertRows(at: recoveredInsertions, with: insertionAnimation)
-            update.moves.forEach { moveRow(at: $0.from, to: $0.to) }
         }
+    }
+
+    // wrapper for applyWithOptimizations, c.f. original animateRowChanges
+    public func animateRowChangesWithOptimizations<T: Collection>(
+        oldData: T,
+        newData: T,
+        deletionAnimation: UITableViewRowAnimation = .automatic,
+        insertionAnimation: UITableViewRowAnimation = .automatic,
+        indexPathTransform: (IndexPath) -> IndexPath = { $0 },
+        maxRowsToCalculateDiffs: Int = Threshold.maxRowsToCalculateDiffs,
+        maxDeletionsPreservingAnimations: Int = Threshold.maxDeletionsPreservingAnimations
+        ) where T.Iterator.Element: Equatable {
+        guard oldData.count + newData.count <= maxRowsToCalculateDiffs else {
+            reloadSections([0], with: .none)
+            return
+        }
+
+        applyWithOptimizations(
+            oldData.extendedDiff(newData),
+            deletionAnimation: deletionAnimation,
+            insertionAnimation: insertionAnimation,
+            indexPathTransform: indexPathTransform,
+            maxDeletionsPreservingAnimations: maxDeletionsPreservingAnimations
+        )
+    }
+}
+
+extension BatchUpdate: CustomDebugStringConvertible {
+    public var debugDescription: String {
+        return """
+        deletions: \(deletions.map {"\($0.section, $0.row)"}),
+        insertions: \(insertions.map {"\($0.section, $0.row)"}),
+        moves: \(moves.map {"\($0.from.section, $0.from.row) -> \($0.to.section, $0.to.row)"})
+        """
     }
 }
--- Example/BigDiffer/ViewController.swift
+++ Example/BigDiffer/ViewController.swift
@@ -2,9 +2,15 @@ import UIKit
 import Differ
 import BigDiffer
 
-final class ViewController: UITableViewController {
+final class ViewController: UITableViewController, UISearchResultsUpdating {
     private var datasource: [String] = [] {
-        didSet {diffAndPatch(old: oldValue, new: datasource)}
+        didSet {filteredDatasource = searchText.isEmpty ? datasource : datasource.filter {$0.contains(searchText)}}
+    }
+    private var searchText: String = "" {
+        didSet {filteredDatasource = searchText.isEmpty ? datasource : datasource.filter {$0.contains(searchText)}}
+    }
+    private var filteredDatasource: [String] = [] {
+        didSet {diffAndPatch(old: oldValue, new: filteredDatasource)}
     }
 
     override func viewDidLoad() {
@@ -14,12 +20,20 @@ final class ViewController: UITableViewController {
 
         navigationController?.navigationBar.titleTextAttributes = [.font: UIFont.systemFont(ofSize: 12)]
 
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Sort", style: .plain, target: self, action: #selector(sortRows))
+
         toolbarItems = [
             UIBarButtonItem(title: "1 Row", style: .plain, target: self, action: #selector(updateTo1Row)),
             UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil),
             UIBarButtonItem(title: "5 Rows", style: .plain, target: self, action: #selector(updateTo5Row)),
             UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil),
             UIBarButtonItem(title: "5000 Rows", style: .plain, target: self, action: #selector(updateTo5000Row))]
+
+        if #available(iOS 11, *) {
+            let sc = UISearchController(searchResultsController: nil)
+            sc.searchResultsUpdater = self
+            navigationItem.searchController = sc
+        }
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -34,14 +48,15 @@ final class ViewController: UITableViewController {
         let diff = old.extendedDiff(new)
         let diffed = Date()
 
-        tableView.applyWithOptimizations(diff, numberOfRows: new.count)
+        // tableView.apply(diff)
+        tableView.applyWithOptimizations(diff)
         let applied = Date()
 
         let measures = String(format: "diff: %.1fs, apply: %.1fs, total: %.1fs",
                               diffed.timeIntervalSince(started),
                               applied.timeIntervalSince(diffed),
                               applied.timeIntervalSince(started))
-        title = "\(datasource.count) Rows (\(measures))"
+        title = "\(filteredDatasource.count) Rows (\(measures))"
     }
 
     @objc private func updateTo1Row() {
@@ -56,18 +71,29 @@ final class ViewController: UITableViewController {
         datasource = Array(Data.gemojiFoodsByPeople.prefix(5000))
     }
 
+    @objc private func sortRows() {
+        filteredDatasource.sort {
+            $0.components(separatedBy: CharacterSet(charactersIn: "abcdefghijklmnopqrstuvwxyz0123456789").inverted).filter {!$0.isEmpty}.joined(separator: " ")
+                < $1.components(separatedBy: CharacterSet(charactersIn: "abcdefghijklmnopqrstuvwxyz0123456789").inverted).filter {!$0.isEmpty}.joined(separator: " ")
+        }
+    }
+
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return datasource.count
+        return filteredDatasource.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: UITableViewCell.self), for: indexPath)
-        cell.textLabel?.text = datasource[indexPath.row]
+        cell.textLabel?.text = filteredDatasource[indexPath.row]
         return cell
+    }
+
+    func updateSearchResults(for searchController: UISearchController) {
+        searchText = searchController.searchBar.text ?? ""
     }
 }
 
