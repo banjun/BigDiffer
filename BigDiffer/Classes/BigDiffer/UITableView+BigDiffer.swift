78137349cd0af2a3258d0a277c2a27a3bd13a97a
BAN Jun <banjun@gmail.com>
2018-05-07 10:45:38 +0900

    make table animations customizable

--- BigDiffer/Classes/BigDiffer/UITableView+BigDiffer.swift
+++ BigDiffer/Classes/BigDiffer/UITableView+BigDiffer.swift
@@ -67,35 +67,46 @@ extension UITableView {
 
     public func apply(
         bigDiff: Changeset,
-        deletionAnimation: UITableViewRowAnimation = .automatic,
-        insertionAnimation: UITableViewRowAnimation = .automatic) {
+        rowDeletionAnimation: UITableViewRowAnimation = .fade,
+        rowInsertionAnimation: UITableViewRowAnimation = .fade,
+        sectionDeletionAnimation: UITableViewRowAnimation = .fade,
+        sectionInsertionAnimation: UITableViewRowAnimation = .fade,
+        sectionReloadingAnimation: UITableViewRowAnimation = .fade) {
         beginUpdates()
         defer {endUpdates()}
 
-        deleteSections(.init(bigDiff.deletedSectionIndices), with: deletionAnimation)
-        deleteRows(at: bigDiff.deletionsInSections, with: deletionAnimation)
+        deleteSections(.init(bigDiff.deletedSectionIndices), with: sectionDeletionAnimation)
+        deleteRows(at: bigDiff.deletionsInSections, with: rowDeletionAnimation)
         bigDiff.sectionMoves.forEach {
             // mixing moveSection and delete/insert rows may cause crash
             // see comments at https://github.com/RxSwiftCommunity/RxDataSources/blob/master/Sources/Differentiator/Diff.swift#L232
-            deleteSections([$0.from], with: deletionAnimation)
-            insertSections([$0.to], with: insertionAnimation)
+            deleteSections([$0.from], with: sectionDeletionAnimation)
+            insertSections([$0.to], with: sectionInsertionAnimation)
             // moveSection($0.from, toSection: $0.to)
         }
-        insertSections(.init(bigDiff.insertedSectionIndices), with: insertionAnimation)
-        insertRows(at: bigDiff.insertionsInSections, with: insertionAnimation)
-        reloadSections(.init(bigDiff.reloadableSectionIndices + bigDiff.fallbackedToReloadSectionIndices), with: .automatic)
+        insertSections(.init(bigDiff.insertedSectionIndices), with: sectionInsertionAnimation)
+        insertRows(at: bigDiff.insertionsInSections, with: rowInsertionAnimation)
+        reloadSections(.init(bigDiff.reloadableSectionIndices + bigDiff.fallbackedToReloadSectionIndices), with: sectionReloadingAnimation)
     }
 
     public func reloadUsingBigDiff<T: BigDiffableSections>(
         old: T,
         new: T,
-        deletionAnimation: UITableViewRowAnimation = .automatic,
-        insertionAnimation: UITableViewRowAnimation = .automatic,
+        rowDeletionAnimation: UITableViewRowAnimation = .fade,
+        rowInsertionAnimation: UITableViewRowAnimation = .fade,
+        sectionDeletionAnimation: UITableViewRowAnimation = .fade,
+        sectionInsertionAnimation: UITableViewRowAnimation = .fade,
+        sectionReloadingAnimation: UITableViewRowAnimation = .fade,
         maxDeletionsPreservingAnimations: Int = Threshold.BigDiffer.maxDeletionsPreservingAnimations) {
         guard let diff = bigDiff(old: old, new: new, maxDeletionsPreservingAnimations: maxDeletionsPreservingAnimations) else {
             reloadData()
             return
         }
-        apply(bigDiff: diff)
+        apply(bigDiff: diff,
+              rowDeletionAnimation: rowDeletionAnimation,
+              rowInsertionAnimation: rowInsertionAnimation,
+              sectionDeletionAnimation: sectionDeletionAnimation,
+              sectionInsertionAnimation: sectionInsertionAnimation,
+              sectionReloadingAnimation: sectionReloadingAnimation)
     }
 }
